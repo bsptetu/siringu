58624dd223543f4a318d144921236fdb9a477310
bsptetu <55188675+bsptetu@users.noreply.github.com>
2021-06-22 22:39:00 +0900

    20210622

---
+++ js/plugins/HyperLink2.js
@@ -0,0 +1,94 @@
+﻿//=============================================================================
+// HyperLink.js
+// PUBLIC DOMAIN
+// ----------------------------------------------------------------------------
+// 2016/12/04 リンクの背景を丸く白で塗りました
+// 2017/02/01 ローカル実行時、標準ブラウザからリンクを開くようにしました
+// 2018/10/12 RPGアツマールでのリンク表示に対応
+//=============================================================================
+
+/*:
+ * @plugindesc ゲーム中に外部ページへのリンクを貼ります。
+ * @author くらむぼん
+ *
+ * @param description
+ * @desc リンクと一緒に表示されるメッセージ。すべてのリンクにおいて共通です
+ * @default Webサイトへのリンク
+ *
+ * @help
+ * プラグインコマンドにより指定したタイミングで、他のページへのリンクを表示します。
+ * 位置は画面中央固定。仕組み上ポップアップブロックされることはないはずです。
+ * 
+ * プラグインコマンド：
+ * link on http://*** タイトル
+ * 　「http://***」へのリンクを「タイトル」という名前で画面に表示します。
+ * link off
+ * 　表示しているリンクを消します。
+ * 
+ * RPGアツマールでの動作：
+ * このプラグインは、RPGアツマール上での利用にも対応しています。
+ * ただし、アツマール上ではアツマール側が用意したウィンドウ上にリンクが表示されます。
+ * また、デザインが少し違う関係でlink onの「タイトル」指定が無視されるようになり、
+ * link offは動作しなくなり、代わりにウィンドウを閉じることでリンクを消せるようになります。
+ * 
+ * ライセンス：
+ * このプラグインの利用法に制限はありません。お好きなようにどうぞ。
+ */
+
+(function() {
+	'use strict';
+	var parameters = PluginManager.parameters('HyperLink');
+	var description = parameters['description'];
+
+	function stopPropagation(event) {
+		event.stopPropagation();
+	}
+
+	Graphics.printLink = function(url, title) {
+		if (this._errorPrinter) {
+			var link = '<a href="index1.html" id="HyperLink" class="saba1"><img src="slight.png"></a><a href="index2.html" id="HyperLink2" class="saba2"><img src="sdarkness.png"></a><a href="index3.html" id="HyperLink3" class="saba3"><img src="sNature.png"></a>';
+			this._errorPrinter.innerHTML = this._makeErrorHtml("", link);
+			var a = document.getElementById('HyperLink');
+			a.addEventListener('mousedown', stopPropagation);
+			a.addEventListener('touchstart', stopPropagation);
+			a.addEventListener('click', function(event) {
+			});
+
+			var a = document.getElementById('HyperLink2');
+			a.addEventListener('mousedown', stopPropagation);
+			a.addEventListener('touchstart', stopPropagation);
+			a.addEventListener('click', function(event) {
+			});
+
+			var a = document.getElementById('HyperLink3');
+			a.addEventListener('mousedown', stopPropagation);
+			a.addEventListener('touchstart', stopPropagation);
+			a.addEventListener('click', function(event) {
+			});
+
+		}
+	};
+
+	Graphics.clearLink = function() {
+		if (this._errorPrinter) {
+			this._errorPrinter.innerHTML = '';
+		}
+	};
+
+	var _Game_Interpreter_pluginCommand = Game_Interpreter.prototype.pluginCommand;
+	Game_Interpreter.prototype.pluginCommand = function(command, args) {
+		_Game_Interpreter_pluginCommand.apply(this, arguments);
+		if (command.toLowerCase() === 'link') {
+			switch (args[0].toLowerCase()) {
+				case 'on':
+					Graphics.printLink(args[1], args[2]);
+					break;
+				case 'off':
+					Graphics.clearLink();
+					break;
+				default:
+					break;
+			}
+		}
+	};
+})();
---
+++ js/plugins/kaihen.js
@@ -0,0 +1,225 @@
+﻿(function() {
+var _prevfunc = Game_Action.prototype.executeHpDamage;
+ Game_Action.prototype.executeHpDamage = function(target, value) {
+ _prevfunc.call(this, target, value);
+$gameVariables.setValue(321,value);
+};
+
+ })();
+
+Game_Battler.prototype.performMiss = function() {
+    $gameVariables.setValue(321,0);
+};
+
+Window_BattleLog.prototype.animationBaseDelay = function() {
+    return 0;
+};
+
+Window_BattleLog.prototype.animationNextDelay = function() {
+    return 0;
+};
+
+Window_BattleLog.prototype.updateWaitCount = function() {
+	var waitMax;
+	var waitMin;
+   	    waitMax = 3 * 100;
+    	waitMin = 1 * 100;
+
+    if (this._waitCount > 0) {
+        this._waitCount -= this.isFastForward() ? waitMax : waitMin;
+        if (this._waitCount < 0) {
+            this._waitCount = 0;
+        }
+    	return true;
+    }
+    return false;
+};
+
+BattleManager.endTurn = function() {
+    this._phase = 'turnEnd';
+    this._preemptive = false;
+    this._surprise = false;
+    this.allBattleMembers().forEach(function(battler) {
+        battler.onTurnEnd();
+        this.refreshStatus();
+        this._logWindow.displayAutoAffectedStatus(battler);
+        this._logWindow.displayRegeneration(battler);
+    }, this);
+    if (this.isForcedTurn()) {
+        //this._turnForced = false;
+    }
+};
+
+BattleManager.refreshStatus = function() {
+    //this._statusWindow.refresh();
+};
+
+Scene_Menu.prototype.start = function() {
+    Scene_MenuBase.prototype.start.call(this);
+    //this._statusWindow.refresh();
+};
+
+Scene_Battle.prototype.refreshStatus = function() {
+    //this._statusWindow.refresh();
+};
+
+
+
+Game_Action.prototype.applyCritical = function(damage) {
+    return damage * 1.5;
+};
+
+Game_Action.prototype.executeDamage = function(target, value) {
+    var result = target.result();
+    if (value === 0) {
+        result.critical = false;
+    }
+    if (this.isHpEffect()) {
+        this.executeHpDamage(target, value);
+     if (target.result().hpDamage === 0) {
+if (this.item().damage.type === 1) {
+        if (this.item().damage.elementId <= 27) {
+            target.startAnimation(64);
+        }
+    }
+    }
+    }
+    if (this.isMpEffect()) {
+        this.executeMpDamage(target, value);
+    }
+};
+
+
+
+Window_BattleStatus.prototype.initialize = function() {
+    var width = this.windowWidth();
+    var height = this.windowHeight();
+    var x = Graphics.boxWidth - width;
+    var y = 1400;Graphics.boxHeight - height;
+    Window_Selectable.prototype.initialize.call(this, x, y, width, height);
+    this.refresh();
+    this.openness = 0;
+};
+
+Window_PartyCommand.prototype.initialize = function() {
+    var y = 720;Graphics.boxHeight - this.windowHeight();
+    Window_Command.prototype.initialize.call(this, 500, y);
+    this.openness = 0;
+    this.deactivate();
+};
+
+Window_ActorCommand.prototype.initialize = function() {
+    var y = 720;Graphics.boxHeight - this.windowHeight();
+    Window_Command.prototype.initialize.call(this, 500, y);
+    this.openness = 0;
+    this.deactivate();
+    this._actor = null;
+};
+
+Window_ActorCommand.prototype.windowWidth = function() {
+    return 200;192;
+};
+
+Window_ActorCommand.prototype.numVisibleRows = function() {
+    return 2;
+};
+
+Window_PartyCommand.prototype.windowWidth = function() {
+    return 200;192;
+};
+
+Window_PartyCommand.prototype.numVisibleRows = function() {
+    return 2;
+};
+
+Window_BattleLog.prototype.initialize = function() {
+    var width = this.windowWidth();
+    var height = this.windowHeight();
+    Window_Selectable.prototype.initialize.call(this, 750, 0, width, height);
+    this.opacity = 0;
+    this._lines = [];
+    this._methods = [];
+    this._waitCount = 0;
+    this._waitMode = '';
+    this._baseLineStack = [];
+    this._spriteset = null;
+    this.createBackBitmap();
+    this.createBackSprite();
+    this.refresh();
+};
+
+Window_Base.prototype.standardFontSize = function() {
+    return 42;
+};
+
+Window_Base.prototype.lineHeight = function() {
+    return 60;
+};
+
+Game_Battler.prototype.refresh = function() {
+    Game_BattlerBase.prototype.refresh.call(this);
+    if (this.hp === 0) {
+        this.addState(this.deathStateId());
+        $gameTemp.reserveCommonEvent(19);
+    } else {
+        this.removeState(this.deathStateId());
+    }
+};
+
+Sprite_Actor.prototype.retreat = function() {
+    this.startMove(300, 0, 30);
+};
+
+Sprite_Actor.prototype.setActorHome = function(index) {
+    this.setHome(850 + index * 32, 900 + index * 48);
+};
+
+Game_Party.prototype.maxBattleMembers = function() {
+    return 10;
+};
+
+Sprite_Actor.prototype.damageOffsetX = function() {
+    return 0;
+};
+
+Sprite_Actor.prototype.damageOffsetY = function() {
+    return 80;
+};
+
+Sprite_Enemy.prototype.damageOffsetX = function() {
+    return 0;
+};
+
+Sprite_Enemy.prototype.damageOffsetY = function() {
+    return 80;
+};
+
+Sprite_Enemy.prototype.startCollapse = function() {
+    this._effectDuration = 32;
+    this._appeared = false;
+};
+
+ImageManager.loadBitmap = function(folder, filename, hue, smooth) {
+    if (filename) {
+        var path = folder + encodeURIComponent(filename) + '.png';
+        var bitmap = this.loadNormalBitmap(path, hue || 0);
+        bitmap.smooth = false;
+        return bitmap;
+    } else {
+        return this.loadEmptyBitmap();
+    }
+};
+
+Window_BattleLog.prototype.messageSpeed = function() {
+    return 0;
+};
+
+(function () {
+  var _render = Graphics.render;
+  Graphics.render = function (stage) {
+    if (this._skipCount < 0) {
+      this._skipCount = 0;
+    }
+    _render.call(this, stage);
+  };
+})();
